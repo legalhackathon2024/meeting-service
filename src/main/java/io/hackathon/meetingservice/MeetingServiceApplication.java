eb619ec83986d2030b26b334b6892241795294db
Grzegorz Oladele <grzegorzoladele@MacBook-Pro-Grzegorz.local>
2024-03-08 19:53:42 +0100

    Init project

---
+++ src/main/java/io/hackathon/meetingservice/MeetingServiceApplication.java
@@ -0,0 +1,13 @@
+package io.hackathon.meetingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingServiceApplication.class, args);
+	}
+
+}
